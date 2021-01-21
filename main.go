a16659765c368e4fa6b76ca1cf8379cc6aaddce1
deepak-gc <arian911@gmail.com>
2021-01-22 00:52:48 +0530

    go mod added

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
     "fmt"
     "net/http"
     "log"
+    "github.com/pborman/uuid"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+
         log.Print("Hi");
+
+        uuidWithHyphen := uuid.NewRandom()
+        fmt.Println(uuidWithHyphen.String())
     })
 
     http.ListenAndServe(":3000", nil)
