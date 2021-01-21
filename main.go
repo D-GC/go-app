9e5f62fbdb103c046d4c927471049258db2afdda
deepak-gc <arian911@gmail.com>
2021-01-21 16:49:09 +0530

    3rd run

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
     "fmt"
     "net/http"
     "log"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
         log.fmt("Hello");
-        mgo.Hello;
     })
 
     http.ListenAndServe(":3000", nil)
